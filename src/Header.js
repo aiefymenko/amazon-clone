4e9437cffbac1c451ef52462256f6644ebbfe5a7
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-10-19 21:00:13 -0400

    Create logo, search input styled coponents

--- src/Header.js
+++ src/Header.js
@@ -6,16 +6,19 @@ function Header() {
     <HeaderBar>
 
     <HeaderLogo>
-
-
+    <img src='https://i0.wp.com/www.dafontfree.co/wp-content/uploads/2021/11/Amazon-Logo-Font-1-scaled.jpg?resize=2560%2C1578' alt='amazon logo'></img>
     </HeaderLogo>
-
+        
         <HeaderAddress>
           <LineOne>Deliver to</LineOne>
           <LineTwo>Select your address</LineTwo>
-
-
         </HeaderAddress>
+        
+        <HeaderSearch>
+          <HeaderSearchInput type='text'>
+
+          </HeaderSearchInput>
+        </HeaderSearch>
 
       </HeaderBar>
   )
@@ -26,9 +29,17 @@ export default Header
 const HeaderBar = styled.div`
   height: 60px;
   background-color: #0F1111;
+  display: flex;
+  align-items: center;
+  color: white;
+
 `
 
 const HeaderLogo = styled.div`
+img {
+  width: 100px;
+  margin-left: 10px;
+}
 `
 
 const HeaderAddress = styled.div`
@@ -38,4 +49,10 @@ const LineOne = styled.div`
 `
 
 const LineTwo = styled.div`
+`
+
+const HeaderSearch = styled.div`
+`
+
+const HeaderSearchInput = styled.input`
 `
