a7945760f42432628af123be7d0a7d6c00456877
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-10-21 15:46:32 -0400

    Add background image for Home component

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Home() {
   return (
-    <div>Home</div>
+    <Container>
+      <Banner>
+
+      </Banner>
+      <Contant>
+
+      </Contant>
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.div`
+`
+
+const Banner = styled.div`
+background-image: url('https://m.media-amazon.com/images/I/61URLJb3ANL._SX3000_.jpg');
+min-height: 600px;
+background-position: center;
+background-size: cover;
+`
+
+const Contant = styled.div`
+`
