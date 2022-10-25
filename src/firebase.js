284b014b92d7c50bf7b7621069ad61b16647d98e
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-10-25 18:53:07 -0500

    Set up firebase db

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyAMptsI63ovrKSNLGq-WDkHMmiouRynp8g",
+  authDomain: "clone-6115a.firebaseapp.com",
+  projectId: "clone-6115a",
+  storageBucket: "clone-6115a.appspot.com",
+  messagingSenderId: "238003366361",
+  appId: "1:238003366361:web:e1efb03fc8968773c951c8",
+  measurementId: "G-9DSDBSPL3K"
+});
+
+const db = firebase.firestore();
+
+export {db} 
