21672a6a6edd62c682d4881eee7e4b48e45ebca6
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-01 19:55:38 -0400

    Import created components

---
+++ src/CartItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function CartItems() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default CartItems
+
+const Container = styled.div`
+`
---
+++ src/CartTotal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+function CartTotal() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default CartTotal
+
+const Container = styled.div`
+`
