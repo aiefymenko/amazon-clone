f3de99863a27e631d99de046a72bba3d64a45cca
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-11-01 20:47:33 -0400

    Add style components to CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,7 +4,27 @@ import styled from 'styled-components';
 function CartItem() {
   return (
    <Container>
-    CartItem
+    <ImageContainer>
+    <img src={''}/>
+    </ImageContainer>
+    <CartItemInfo>
+      <CartItemDescription>
+        <h2></h2>
+      </CartItemDescription>
+      <CartItemBottom>
+      <CartItemQuantity>
+     5
+      </CartItemQuantity>
+      <CartItemDelete>
+        Delete
+      </CartItemDelete>
+      </CartItemBottom>
+
+    </CartItemInfo>
+
+    <CartItemPrice>
+    $1499
+    </CartItemPrice>
     </Container>
   )
 }
@@ -13,3 +33,29 @@ export default CartItem
 
 const Container = styled.div`
 `
+
+const ImageContainer = styled.img`
+`
+const CartItemInfo = styled.div`
+`
+
+const CartItemDescription = styled.div`
+`
+
+const CartItemBottom = styled.div`
+`
+
+const CartItemQuantity = styled.div`
+`
+
+const CartItemDelete = styled.div`
+`
+
+const CartItemPrice = styled.div`
+`
+
+
+
+
+
+
